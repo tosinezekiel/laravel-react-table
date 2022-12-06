d275d1a128f9b0975adb977ff37c342ff1a1a347
tosinezekiel <tosin@circlelinkhealth.com>
2022-12-06 11:13:07 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div>
+        <h1 className="text-3xl text-red-500 font-bold underline">
+          Hello world!
+        </h1>
+    </div>
+  )
+}
+
+export default App
